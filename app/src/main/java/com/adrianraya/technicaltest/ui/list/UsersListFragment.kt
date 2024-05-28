233c5c1925fb69490567a4235363466dc3148292
AdrianRaya <adrian.raya-hernandez@domakaba-external.com>
2024-05-28 21:35:30 +0200

    New fragment main

--- app/src/main/java/com/adrianraya/technicaltest/ui/list/UsersListFragment.kt
+++ app/src/main/java/com/adrianraya/technicaltest/ui/list/UsersListFragment.kt
@@ -3,12 +3,10 @@ package com.adrianraya.technicaltest.ui.list
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import androidx.activity.addCallback
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.adrianraya.technicaltest.R
-import com.adrianraya.technicaltest.data.alert
 import com.adrianraya.technicaltest.databinding.FragmentUsersListBinding
 import com.adrianraya.technicaltest.ui.common.launchAndCollect
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,22 +22,13 @@ class UsersListFragment : Fragment(R.layout.fragment_users_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        requireActivity().onBackPressedDispatcher.addCallback(this) {
-            askBeforeClose()
-        }
         usersState = buildUsersListState()
 
         FragmentUsersListBinding.bind(view).apply {
             rvUserList.adapter = adapter
 
-            lyToolbar.btBack.setImageDrawable(
-                ContextCompat.getDrawable(
-                    requireContext(),
-                    R.drawable.ic_close
-                )
-            )
             lyToolbar.tvTitleToolbar.text = getString(R.string.title_toolbar_user_list)
-            lyToolbar.btBack.setOnClickListener { askBeforeClose() }
+            lyToolbar.btBack.setOnClickListener {  findNavController().popBackStack() }
 
             viewLifecycleOwner.launchAndCollect(viewModel.state) {
                 loading = it.loading
@@ -49,18 +38,6 @@ class UsersListFragment : Fragment(R.layout.fragment_users_list) {
             }
         }
         viewModel.onUiReady()
-    }
-
-    private fun askBeforeClose() {
-        alert {
-            setTitle(R.string.app_name)
-            setMessage(R.string.are_you_sure_question_close)
-            setCancelable(false)
-            setPositiveButton(R.string.common_yes) { _, _ ->
-                requireActivity().finish()
-            }
-            setNegativeButton(R.string.common_no, null)
-        }.show()
     }
 
     override fun onDestroy() {
