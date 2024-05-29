51000e811753049f7022b9b08cde57a12789858b
AdrianRaya <adrian.raya-hernandez@domakaba-external.com>
2024-05-29 18:35:31 +0200

    Fix edit text

--- app/src/main/java/com/adrianraya/technicaltest/ui/search/SearchUserFragment.kt
+++ app/src/main/java/com/adrianraya/technicaltest/ui/search/SearchUserFragment.kt
@@ -33,8 +33,7 @@ class SearchUserFragment : Fragment(R.layout.fragment_search_user) {
                 override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
                 override fun afterTextChanged(p0: Editable?) {
-                    userId = if(etIdUser.text.toString().trim() == "") 0
-                    else etIdUser.text.toString().trim().toInt()
+                    userId = etIdUser.text.toString().trim().toIntOrNull() ?: 0
                 }
             })
 
