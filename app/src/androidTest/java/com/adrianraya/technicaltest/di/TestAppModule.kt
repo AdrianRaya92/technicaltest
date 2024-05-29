b202df6e1a9bade4ac1cd4532d95a394ea55e689
AdrianRaya <adrian.raya-hernandez@domakaba-external.com>
2024-05-29 17:58:48 +0200

    Instrumentation espresso test

---
+++ app/src/androidTest/java/com/adrianraya/technicaltest/di/TestAppModule.kt
@@ -0,0 +1,58 @@
+package com.adrianraya.technicaltest.di
+
+import android.app.Application
+import androidx.room.Room
+import com.adrianraya.technicaltest.data.database.UsersDatabase
+import com.adrianraya.technicaltest.data.server.UserAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [AppModule::class]
+)
+object TestAppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application) = Room.inMemoryDatabaseBuilder(
+        app,
+        UsersDatabase::class.java
+    ).build()
+
+    @Provides
+    @Singleton
+    fun provideUsersDao(db: UsersDatabase) = db.usersDao()
+
+    @Provides
+    @Singleton
+    @ApiUrl
+    fun provideApiUrl(): String = "http://localhost:8080"
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient = HttpLoggingInterceptor().run {
+        level = HttpLoggingInterceptor.Level.BODY
+        OkHttpClient.Builder().addInterceptor(this).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemoteService(@ApiUrl apiUrl: String, okHttpClient: OkHttpClient): UserAPI {
+        return Retrofit.Builder()
+            .baseUrl(apiUrl)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create()
+    }
+}
